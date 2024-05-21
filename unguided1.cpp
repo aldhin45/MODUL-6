18948fb6eea25bb7ef34b35af87880c97e8bfb08
aldhin45 <64036580+aldhin45@users.noreply.github.com>
2024-05-21 19:07:06 +0700

    Add files via upload

---
+++ unguided1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+bool SystemPalindrom2311102023(const string &input)
+{
+    stack<char> charCharacters;
+    for (char c : input)
+    {
+        charCharacters.push(c);
+    }
+    for (char c : input)
+    {
+        if (c != charCharacters.top())
+        {
+            return false;
+        }
+        charCharacters.pop();
+    }
+    return true;
+}
+int main()
+{
+    string input;
+    cout << "Masukan Kalimat: ";
+    getline(cin, input);
+    if (SystemPalindrom2311102023(input))
+    {
+        cout << "Kalimat tersebut adalah Palindrom" << endl;
+    }
+    else
+    {
+        cout << "Kalimat tersebut bukan Palindrom" << endl;
+    }
+    return 0;
+}
